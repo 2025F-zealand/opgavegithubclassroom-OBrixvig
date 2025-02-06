dfceebac03178b755471607347063b4fe1323981
Oliver Colding Brixvig <olb005@edu.zealand.dk>
2025-02-06 12:46:17 +0100

    Oprettet Demo

---
+++ MyFirstGitHubClassRoomConsoleApp60/Demo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstGitHubClassRoomConsoleApp60
+{
+    internal class Demo
+    {
+        private string _demu;
+        public int Demos { get; set; }
+    }
+}
